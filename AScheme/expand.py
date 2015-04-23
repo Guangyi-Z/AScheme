b75b301140665cf09d56309f47a960376ebffbb3
Jan Fan <guangyizhang.jan@gmail.com>
2015-04-23 15:57:12 +0800

    refactor expand

---
+++ AScheme/expand.py
@@ -0,0 +1,76 @@
+
+from util import isa, require, is_pair
+from symbol import _quote, _if, _set, _define, _lambda, _begin, _definemacro, _quasiquote, _unquote, _unquotesplicing
+from symbol import Symbol, Sym
+from macro import macro_table
+from eval import eval
+
+################ expand
+
+def expand(x, toplevel=False):
+    "Walk tree of x, making optimizations/fixes, and signaling SyntaxError."
+    require(x, x!=[])                    # () => Error
+    if not isa(x, list):                 # constant => unchanged
+        return x
+    elif x[0] is _quote:                 # (quote exp)
+        require(x, len(x)==2)
+        return x
+    elif x[0] is _if:
+        if len(x)==3: x = x + [None]     # (if t c) => (if t c None)
+        require(x, len(x)==4)
+        return map(expand, x)
+    elif x[0] is _set:
+        require(x, len(x)==3);
+        var = x[1]                       # (set! non-var exp) => Error
+        require(x, isa(var, Symbol), "can set! only a symbol")
+        return [_set, var, expand(x[2])]
+    elif x[0] is _define or x[0] is _definemacro:
+        require(x, len(x)>=3)
+        _def, v, body = x[0], x[1], x[2:]
+        if isa(v, list) and v:           # (define (f args) body)
+            f, args = v[0], v[1:]        #  => (define f (lambda (args) body))
+            return expand([_def, f, [_lambda, args]+body])
+        else:
+            require(x, len(x)==3)        # (define non-var/list exp) => Error
+            require(x, isa(v, Symbol), "can define only a symbol")
+            exp = expand(x[2])
+            if _def is _definemacro:
+                require(x, toplevel, "define-macro only allowed at top level")
+                proc = eval(exp)
+                require(x, callable(proc), "macro must be a procedure")
+                macro_table[v] = proc    # (define-macro v proc)
+                return None              #  => None; add v:proc to macro_table
+            return [_define, v, exp]
+    elif x[0] is _begin:
+        if len(x)==1: return None        # (begin) => None
+        else: return [expand(xi, toplevel) for xi in x]
+    elif x[0] is _lambda:                # (lambda (x) e1 e2)
+        require(x, len(x)>=3)            #  => (lambda (x) (begin e1 e2))
+        vars, body = x[1], x[2:]
+        require(x, (isa(vars, list) and all(isa(v, Symbol) for v in vars))
+                or isa(vars, Symbol), "illegal lambda argument list")
+        exp = body[0] if len(body) == 1 else [_begin] + body
+        return [_lambda, vars, expand(exp)]
+    elif x[0] is _quasiquote:            # `x => expand_quasiquote(x)
+        require(x, len(x)==2)
+        return expand_quasiquote(x[1])
+    elif isa(x[0], Symbol) and x[0] in macro_table:
+        return expand(macro_table[x[0]](*x[1:]), toplevel) # (m arg...)
+    else:                                #        => macroexpand if m isa macro
+        return map(expand, x)            # (f arg...) => expand each
+
+_append, _cons, _let = map(Sym, "append cons let".split())
+
+def expand_quasiquote(x):
+    """Expand `x => 'x; `,x => x; `(,@x y) => (append x y) """
+    if not is_pair(x):
+        return [_quote, x]
+    require(x, x[0] is not _unquotesplicing, "can't splice here")
+    if x[0] is _unquote:
+        require(x, len(x)==2)
+        return x[1]
+    elif is_pair(x[0]) and x[0][0] is _unquotesplicing:
+        require(x[0], len(x[0])==2)
+        return [_append, x[0][1], expand_quasiquote(x[1:])]
+    else:
+        return [_cons, expand_quasiquote(x[0]), expand_quasiquote(x[1:])]
--- AScheme/lispy.py
+++ AScheme/lispy.py
@@ -11,9 +11,9 @@ from util import isa, to_string, require, is_pair, cons
 from symbol import Symbol, Sym, eof_object
 from symbol import _quote, _if, _set, _define, _lambda, _begin, _definemacro, _quasiquote, _unquote, _unquotesplicing
 from token import InPort
-from env import Env, global_env
 from parser import read
 from eval import eval
+from expand import expand
 
 ################ parse, read, and user interaction
 
@@ -22,88 +22,6 @@ def parse(inport):
     # Backwards compatibility: given a str, convert it to an InPort
     if isinstance(inport, str): inport = InPort(StringIO.StringIO(inport))
     return expand(read(inport), toplevel=True)
-
-################ expand
-
-def expand(x, toplevel=False):
-    "Walk tree of x, making optimizations/fixes, and signaling SyntaxError."
-    require(x, x!=[])                    # () => Error
-    if not isa(x, list):                 # constant => unchanged
-        return x
-    elif x[0] is _quote:                 # (quote exp)
-        require(x, len(x)==2)
-        return x
-    elif x[0] is _if:
-        if len(x)==3: x = x + [None]     # (if t c) => (if t c None)
-        require(x, len(x)==4)
-        return map(expand, x)
-    elif x[0] is _set:
-        require(x, len(x)==3);
-        var = x[1]                       # (set! non-var exp) => Error
-        require(x, isa(var, Symbol), "can set! only a symbol")
-        return [_set, var, expand(x[2])]
-    elif x[0] is _define or x[0] is _definemacro:
-        require(x, len(x)>=3)
-        _def, v, body = x[0], x[1], x[2:]
-        if isa(v, list) and v:           # (define (f args) body)
-            f, args = v[0], v[1:]        #  => (define f (lambda (args) body))
-            return expand([_def, f, [_lambda, args]+body])
-        else:
-            require(x, len(x)==3)        # (define non-var/list exp) => Error
-            require(x, isa(v, Symbol), "can define only a symbol")
-            exp = expand(x[2])
-            if _def is _definemacro:
-                require(x, toplevel, "define-macro only allowed at top level")
-                proc = eval(exp)
-                require(x, callable(proc), "macro must be a procedure")
-                macro_table[v] = proc    # (define-macro v proc)
-                return None              #  => None; add v:proc to macro_table
-            return [_define, v, exp]
-    elif x[0] is _begin:
-        if len(x)==1: return None        # (begin) => None
-        else: return [expand(xi, toplevel) for xi in x]
-    elif x[0] is _lambda:                # (lambda (x) e1 e2)
-        require(x, len(x)>=3)            #  => (lambda (x) (begin e1 e2))
-        vars, body = x[1], x[2:]
-        require(x, (isa(vars, list) and all(isa(v, Symbol) for v in vars))
-                or isa(vars, Symbol), "illegal lambda argument list")
-        exp = body[0] if len(body) == 1 else [_begin] + body
-        return [_lambda, vars, expand(exp)]
-    elif x[0] is _quasiquote:            # `x => expand_quasiquote(x)
-        require(x, len(x)==2)
-        return expand_quasiquote(x[1])
-    elif isa(x[0], Symbol) and x[0] in macro_table:
-        return expand(macro_table[x[0]](*x[1:]), toplevel) # (m arg...)
-    else:                                #        => macroexpand if m isa macro
-        return map(expand, x)            # (f arg...) => expand each
-
-_append, _cons, _let = map(Sym, "append cons let".split())
-
-def expand_quasiquote(x):
-    """Expand `x => 'x; `,x => x; `(,@x y) => (append x y) """
-    if not is_pair(x):
-        return [_quote, x]
-    require(x, x[0] is not _unquotesplicing, "can't splice here")
-    if x[0] is _unquote:
-        require(x, len(x)==2)
-        return x[1]
-    elif is_pair(x[0]) and x[0][0] is _unquotesplicing:
-        require(x[0], len(x[0])==2)
-        return [_append, x[0][1], expand_quasiquote(x[1:])]
-    else:
-        return [_cons, expand_quasiquote(x[0]), expand_quasiquote(x[1:])]
-
-def let(*args):
-    args = list(args)
-    x = [_let] + args
-    require(x, len(args)>1)
-    bindings, body = args[0], args[1:]
-    require(x, all(isa(b, list) and len(b)==2 and isa(b[0], Symbol)
-                   for b in bindings), "illegal binding list")
-    vars, vals = zip(*bindings)
-    return [[_lambda, list(vars)]+map(expand, body)] + map(expand, vals)
-
-macro_table = {_let:let} ## More macros can go here
 
 eval(parse("""(begin
 
